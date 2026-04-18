656d9582cf5cef5878f0d1c11db99dc2e606bf62
hinriksnaer <hgudmund@redhat.com>
2026-04-18 00:52:39 -0400

    Container: fix SSH agent forwarding, bake GitHub host keys, fix script paths
    - Move known_hosts to /etc/ssh/ssh_known_hosts (works regardless of container user)
    - Remove .ssh volume mount (was overriding baked-in known_hosts)
    - Only mount SSH agent socket into container at /tmp/ssh-agent.sock
    - Fix hyprpunk-container script: resolve symlink for REPO_DIR and SCRIPT_DIR
    - Full chain verified: desktop -> proton-pass -> SSH forward -> ibm-kaiba -> container -> GitHub

--- containers/default.nix
+++ containers/default.nix
@@ -20,18 +20,31 @@ let
     HOME=$out/home/softmax \
       bash $out/home/softmax/hyprpunk-nix/bootstrap.sh
 
-    # SSH directory
+    # SSH directory with known hosts baked in
     mkdir -p $out/home/softmax/.ssh
     chmod 700 $out/home/softmax/.ssh
+    cat > $out/home/softmax/.ssh/known_hosts << 'HOSTS'
+github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
+HOSTS
+    chmod 644 $out/home/softmax/.ssh/known_hosts
   '';
 
   etcDir = pkgs.runCommand "hyprpunk-etc" {} ''
-    mkdir -p $out/etc
+    mkdir -p $out/etc/ssh
     echo "root:x:0:0:root:/root:/bin/bash" > $out/etc/passwd
     echo "softmax:x:1000:1000::/home/softmax:${pkgs.fish}/bin/fish" >> $out/etc/passwd
     echo "root:x:0:" > $out/etc/group
     echo "users:x:1000:softmax" >> $out/etc/group
     echo "hosts: files dns" > $out/etc/nsswitch.conf
+
+    # System-wide known hosts (works regardless of which user runs the container)
+    cat > $out/etc/ssh/ssh_known_hosts << 'HOSTS'
+github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
+HOSTS
   '';
 
 in
